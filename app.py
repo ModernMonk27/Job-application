11024ceb297fca5719784782370f13a0323574ed
monk <lakshmiaravind.atom@gmail.com>
2023-10-03 08:13:42 +0530

    created a database file using flask_sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config["SECRET_KEY"] = "aravind123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+db = SQLAlchemy(app)
+
+class Form(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.String(80))
+    last = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+@app.route("/", methods=["GET", "POST"])
 def home():
+    print(request.method)
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        print(occupation)
+
+
     return render_template("index.html")
 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
 
-app.run(debug=True, port=5001)
+
