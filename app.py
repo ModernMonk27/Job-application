455ac7c626c3c3a51d86fced6372eecc21cf4f7f
monk <lakshmiaravind.atom@gmail.com>
2023-10-03 10:08:39 +0530

    sent email using flask-mail module

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_mail import Mail,Message
+
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "aravind123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smpt.gmail.com"
+app.config["MAIL_PASSWORD"] = "offhtakpzwweuvvm"
+app.config["MAIL_USERNAME"] = "lakshmiaravind.atom@gmail.com"
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_PORT"] = 465
 
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 class Form(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.String(80))
-    last = db.Column(db.String(80))
+    last = db.Column(db.Integer)
+
     email = db.Column(db.String(80))
     date = db.Column(db.Date)
     occupation = db.Column(db.String(80))
@@ -24,8 +34,20 @@ def home():
         email = request.form["email"]
         phone = request.form["phone"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date,"%Y-%m-%d")
         occupation = request.form["occupation"]
         print(occupation)
+        form = Form(first=name,email=email,date=date_obj,occupation = occupation)
+        db.session.add(form)
+        db.session.commit()
+        message_body="hi"
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[mail],
+                          body=message_body)
+        mail.send(message)
+        flash(f"{name} your application is sent succesfully", "success")
 
 
     return render_template("index.html")
